fabcedebb528ddb7183574a4df5bf4c255d51fc2
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-26 10:45:58 -0500

    Added API routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,17 @@ import "./App.css";
 import { Jumbotron } from "reactstrap";
 import { BrowserRouter, Route } from "react-router-dom";
 import NavigationBar from "./components/Navbar"
+import ViewAllCharacters from './pages/ViewAllCharacters'
 class App extends Component {
   render() {
     return (
       <div className="App ">
         <BrowserRouter>
           <NavigationBar className="">Navbar here</NavigationBar>
-          <Route path="/" component={() => <div>Hello World</div>} />
-          <Route path="/mychars" component={() => <div>My Characters</div>} />
-          <Route path="/new" component={() => <div>Create a character</div>} />
-          <Route
-            path="/edit/:id"
-            component={() => <div>Edit A character</div>}
-          />
+            <Route exact path="/" component={ViewAllCharacters} />
+            <Route exact path="/mychars" component={'ewy'}/>
+            <Route exact path="/new" component={'o'}/>
+            <Route exact path="/edit/:id" component={'Yee'}/>
         </BrowserRouter>
       </div>
     );
